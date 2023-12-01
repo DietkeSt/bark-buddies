0a4f61fb8230463097fe731dfed95c225b101092
DietkeSt <>
2023-12-01 15:02:24 +0000

    Adjust the comment section for admin

--- booking/admin.py
+++ booking/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Service
+from .models import Service, Comment
 from django_summernote.admin import SummernoteModelAdmin
 
 
@@ -11,3 +11,15 @@ class ServiceAdmin(SummernoteModelAdmin):
     prepopulated_fields = {'slug': ('title',)}
     list_filter = ('status', 'title')
     summernote_fields = ('content')
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+
+    list_display = ('name', 'body', 'post', 'created_on', 'approved')
+    list_filter = ('approved', 'created_on')
+    search_fields = ('name', 'email', 'body')
+    action = ['approve_comments']
+
+    def approve_comments(self, request, queryset):
+        queryset.update(approved=True)
