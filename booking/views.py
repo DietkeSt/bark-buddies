e3b84dad15c013c9e5234ca658e8dc4d8eea3caa
DietkeSt <>
2023-12-01 15:29:06 +0000

    Add ServiceList model

--- booking/views.py
+++ booking/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.views import generic
+from .models import Service
 
-# Create your views here.
+
+class ServiceList(generic.ListView):
+    model = Service
+    queryset = Service.objects.filter(status=1).oder_by('title')
+    template_name = 'index.html'
+    paginate_by = 1
