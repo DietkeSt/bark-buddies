1964d189fd023da63ae63d4bebf386c59389ab2a
DietkeSt <>
2023-12-01 14:43:29 +0000

    Add serch, list display and filter to ServiceAdmin

--- booking/admin.py
+++ booking/admin.py
@@ -6,4 +6,8 @@ from django_summernote.admin import SummernoteModelAdmin
 @admin.register(Service)
 class ServiceAdmin(SummernoteModelAdmin):
 
+    list_display = ('title', 'slug', 'status')
+    search_fields = ['title', 'content']
+    prepopulated_fields = {'slug': ('title',)}
+    list_filter = ('status', 'title')
     summernote_fields = ('content')
