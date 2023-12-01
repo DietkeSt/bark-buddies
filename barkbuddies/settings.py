05bcd187981ed5c096d6519bc20dbb97f9fb104c
DietkeSt <>
2023-12-01 12:27:19 +0000

    Add Heroku to allowed hosts

--- barkbuddies/settings.py
+++ barkbuddies/settings.py
@@ -29,7 +29,11 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['8000-dietkest-bark-buddies-q0kxl92vyc.us2.codeanyapp.com']
+ALLOWED_HOSTS = [
+    '8000-dietkest-bark-buddies-q0kxl92vyc.us2.codeanyapp.com',
+    'bark-buddies.herokuapp.com',
+    'localhost',
+]
 
 
 # Application definition
