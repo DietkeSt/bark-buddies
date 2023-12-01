94d1e684ddb88dc8245480a9b45009260c5ca498
DietkeSt <>
2023-12-01 15:06:44 +0000

    Add price to service model

--- booking/models.py
+++ booking/models.py
@@ -2,13 +2,17 @@ from django.db import models
 from django.contrib.auth.models import User
 from cloudinary.models import CloudinaryField
 
-STATUS = ((0, "Draft")), (1, "Published")
+STATUS = (
+    (0, "Draft"),
+    (1, "Published"),
+)
 
 
 class Service(models.Model):
     title = models.CharField(max_length=200, unique=True)
     slug = models.SlugField(max_length=200, unique=True)
     content = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2, default=0)
     featured_image = CloudinaryField('image', default='placeholder')
     status = models.IntegerField(choices=STATUS, default=0)
     likes = models.ManyToManyField(
