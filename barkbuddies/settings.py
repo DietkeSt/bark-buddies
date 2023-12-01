d2939741c9b81a44649a1f2d92947373e68961b9
DietkeSt <>
2023-12-01 14:26:43 +0000

    Add ServiceAdmin Modal

--- barkbuddies/settings.py
+++ barkbuddies/settings.py
@@ -47,7 +47,7 @@ INSTALLED_APPS = [
     'cloudinary_storage',
     'django.contrib.staticfiles',
     'cloudinary',
-    'django_summernote'
+    'django_summernote',
     'booking',
 ]
 
--- booking/admin.py
+++ booking/admin.py
@@ -1,4 +1,9 @@
 from django.contrib import admin
 from .models import Service
+from django_summernote.admin import SummernoteModelAdmin
 
-admin.site.register(Service)
+
+@admin.register(Service)
+class ServiceAdmin(SummernoteModelAdmin):
+
+    summernote_fields = ('content')
