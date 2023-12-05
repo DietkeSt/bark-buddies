e9bd949973a21176f05495ff8ff0b662078e70e9
DietkeSt <>
2023-12-05 21:15:34 +0000

    Import Calendar and AvailableTime to admin

--- booking/admin.py
+++ booking/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Service, Comment
+from .models import Service, Comment, Calendar, AvailableTime
 from django_summernote.admin import SummernoteModelAdmin
 
 
@@ -23,3 +23,14 @@ class CommentAdmin(admin.ModelAdmin):
 
     def approve_comments(self, request, queryset):
         queryset.update(approved=True)
+
+
+@admin.register(Calendar)
+class CalendarAdmin(admin.ModelAdmin):
+    list_display = ('service',)
+    search_fields = ['service__title']
+
+
+@admin.register(AvailableTime)
+class AvailableTimeAdmin(admin.ModelAdmin):
+    list_display = ('day_of_week', 'start_time', 'end_time')
