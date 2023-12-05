2e560db518d6835577673d6ea5d1ae33688b8594
DietkeSt <>
2023-12-05 20:43:44 +0000

    Add the service detail url pattern

--- booking/urls.py
+++ booking/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 
 
 urlpatterns = [
-    path('', views.ServiceList.as_view(), name='home')
+    path('', views.ServiceList.as_view(), name='home'),
+    path('<slug:slug>/', views.ServiceDetail.as_view(), name='service_detail'),
 ]
