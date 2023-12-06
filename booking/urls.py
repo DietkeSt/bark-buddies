e35cbe8fb4b4052d4b7515457edcc6b4ffd99d0e
DietkeSt <>
2023-12-06 17:46:23 +0000

    Adjust urlpatterns to shorten them

--- booking/urls.py
+++ booking/urls.py
@@ -1,8 +1,9 @@
 from . import views
+from .views import ServiceList, ServiceDetail
 from django.urls import path
 
 
 urlpatterns = [
-    path('', views.ServiceList.as_view(), name='home'),
-    path('<slug:slug>/', views.ServiceDetail.as_view(), name='service_detail'),
+    path('', ServiceList.as_view(), name='home'),
+    path('<slug:slug>/', ServiceDetail.as_view(), name='service_detail'),
 ]
