bb7e8a9bc33dce2d93ca87120caa80ad02e88c48
DietkeSt <>
2023-12-12 18:35:05 +0000

    Adjust BookingForm to add available slots

--- booking/forms.py
+++ booking/forms.py
@@ -9,6 +9,11 @@ class CommentForm(forms.ModelForm):
 
 
 class BookingForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        available_slots = kwargs.pop('available_slots', [])
+        super(BookingForm, self).__init__(*args, **kwargs)
+        self.fields['time_slot'].choices = available_slots
+
     class Meta:
         model = Booking
         fields = ['start_date', 'end_date', 'time_slot', 'comments']
