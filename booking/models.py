8005ad5095792ec2da5814932e33f6a87ae0743a
DietkeSt <>
2023-12-12 17:27:28 +0000

    Add Booking model

--- booking/models.py
+++ booking/models.py
@@ -2,30 +2,14 @@ import datetime
 from django.db import models
 from django.contrib.auth.models import User
 from cloudinary.models import CloudinaryField
+from django.conf import settings
+from django import forms
+from django.utils import timezone
 
 STATUS = (
     (0, "Draft"),
     (1, "Published"),
 )
-
-
-class AvailableTime(models.Model):
-    service = models.ForeignKey(
-        'Service', on_delete=models.CASCADE, blank=True, null=True)
-    day_of_week = models.IntegerField(choices=[
-        (1, "Monday"),
-        (2, "Tuesday"),
-        (3, "Wednesday"),
-        (4, "Thursday"),
-        (5, "Friday"),
-        (6, "Saturday"),
-        (7, "Sunday"),
-    ])
-    start_time = models.TimeField()
-    end_time = models.TimeField()
-
-    def __str__(self):
-        return f"{self.get_day_of_week_display()} {self.start_time} - {self.end_time}"
 
 
 class Service(models.Model):
@@ -63,3 +47,20 @@ class Comment(models.Model):
 
     def __str__(self):
         return f"Comment {self.body} by {self.name}"
+
+
+class Booking(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    service = models.ForeignKey(Service, on_delete=models.CASCADE)
+    start_date = models.DateField()
+    end_date = models.DateField()
+    time_slot = models.CharField(max_length=10, choices=[(
+        'Morning', 'Morning'), ('Noon', 'Noon'), ('Evening', 'Evening')], default='Morning')
+    comments = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return f'{self.user} booked {self.service} from {self.start_date} to {self.end_date} at {self.time_slot}'
+
+    # Add a method to check if the booking can be cancelled
+    def can_cancel(self):
+        return timezone.now() < (self.start_date - timezone.timedelta(days=1))
