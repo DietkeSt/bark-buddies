85070e7d402b03e322f3cd6fe0efe34ceb711ac0
DietkeSt <>
2023-12-14 21:07:02 +0000

    Add views for available slots, BookingView, view_bookings, cancel_bookings, add_comment

--- booking/views.py
+++ booking/views.py
@@ -1,7 +1,13 @@
+import datetime
+import logging
+from django import forms
 from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.decorators import login_required
 from django.views import generic, View
-from .models import Service, Booking
+from django.http import HttpResponseRedirect
+from django.contrib.admin.views.decorators import staff_member_required
+from django.contrib import messages
+from .models import Service, Booking, TimeSlot, Comment
 from .forms import CommentForm, BookingForm
 from django.utils import timezone
 
@@ -19,9 +25,8 @@ class ServiceDetail(View):
         service = get_object_or_404(queryset, slug=slug)
         comments = service.comments.filter(
             approved=True).order_by('created_on')
-        liked = False
-        if service.likes.filter(id=self.request.user.id).exists():
-            liked = True
+
+        booking_form = BookingForm(available_slots=get_available_slots())
 
         return render(
             request,
@@ -30,20 +35,16 @@ class ServiceDetail(View):
                 "service": service,
                 "comments": comments,
                 "commented": False,
-                "liked": liked,
-                "comment_form": CommentForm()
+                "comment_form": CommentForm(),
+                "booking_form": booking_form
             },
         )
-
 
     def post(self, request, slug, *args, **kwargs):
         queryset = Service.objects.filter(status=1)
         service = get_object_or_404(queryset, slug=slug)
         comments = service.comments.filter(
             approved=True).order_by('created_on')
-        liked = False
-        if service.likes.filter(id=self.request.user.id).exists():
-            liked = True
 
         comment_form = CommentForm(data=request.POST)
 
@@ -63,10 +64,57 @@ class ServiceDetail(View):
                 "service": service,
                 "comments": comments,
                 "commented": True,
-                "liked": liked,
                 "comment_form": CommentForm()
             },
         )
+
+
+def get_available_slots():
+    today = timezone.now().date()
+    slots = TimeSlot.objects.all()
+    available_slots = []
+    
+    for slot in slots:
+        # Check for bookings that overlap with 'today'
+        overlapping_bookings = Booking.objects.filter(
+            time_slot=slot,
+            start_date__lte=today,  # Bookings that start on or before today
+            end_date__gte=today     # and end on or after today
+        ).count()
+
+        if overlapping_bookings < slot.limit:
+            available_slots.append(
+                (slot.id, f"{slot.time_of_day} (Available)"))
+        else:
+            available_slots.append((slot.id, f"{slot.time_of_day} (Full)"))
+
+    return available_slots
+
+
+class BookingView(View):
+    template_name = 'booking_page.html'
+
+    def get(self, request, *args, **kwargs):
+        service_id = self.kwargs.get('service_id')
+        service = get_object_or_404(Service, id=service_id)
+        form = BookingForm(available_slots=get_available_slots())
+        return render(request, self.template_name, {'form': form, 'service': service})
+
+    def post(self, request, *args, **kwargs):
+        service_id = self.kwargs.get('service_id')
+        service = get_object_or_404(Service, id=service_id)
+        form = BookingForm(request.POST, available_slots=get_available_slots())
+
+        if form.is_valid():
+            booking = form.save(commit=False)
+            booking.user = request.user
+            booking.service = service
+            booking.save()
+            messages.success(request, 'Booking successful.')
+            return redirect('view_bookings')  # Redirect to the view bookings page
+
+        messages.error(request, 'There was an error with your booking.')
+        return render(request, self.template_name, {'form': form, 'service': service})
 
 
 @login_required
@@ -81,7 +129,6 @@ def book_service(request, service_id):
             # Check if the selected slot is marked as full
             selected_slot = TimeSlot.objects.get(id=booking.time_slot.id)
             if Booking.objects.filter(time_slot=selected_slot, start_date=booking.start_date).count() >= selected_slot.limit:
-                # Handle the full slot case
                 return render(request, 'booking_form.html', {
                     'form': form,
                     'service': service,
@@ -91,7 +138,6 @@ def book_service(request, service_id):
             booking.user = request.user
             booking.service = service
             booking.save()
-            # Redirect to booking list
             return redirect('view_bookings')
     else:
         form = BookingForm(available_slots=get_available_slots())
@@ -99,34 +145,53 @@ def book_service(request, service_id):
     return render(request, 'booking_form.html', {'form': form, 'service': service})
 
 
-def get_available_slots():
-    today = timezone.now().date()
-    slots = TimeSlot.objects.all()
-    available_slots = []
-    for slot in slots:
-        booked_count = Booking.objects.filter(
-            time_slot=slot, date=today).count()
-        if booked_count < slot.limit:
-            available_slots.append(
-                (slot.id, f"{slot.time_of_day} (Available)"))
-        else:
-            available_slots.append((slot.id, f"{slot.time_of_day} (Full)"))
-    return available_slots
-
-
 @login_required
 def view_bookings(request):
-    bookings = Booking.objects.filter(user=request.user)
-    return render(request, 'view_bookings.html', {'bookings': bookings})
+    bookings = Booking.objects.filter(user=request.user).order_by('start_date')
+    user_comments = Comment.objects.filter(
+        name=request.user.username)
+
+    if request.method == 'POST':
+        comment_form = CommentForm(request.POST)
+        if comment_form.is_valid():
+            comment = comment_form.save(commit=False)
+            comment.name = request.user.username
+            comment.email = request.user.email
+            comment.save()
+            messages.success(request, 'Review added successfully.')
+            return redirect('view_bookings')
+    else:
+        comment_form = CommentForm()
+
+    return render(request, 'view_bookings.html', {
+        'bookings': bookings,
+        'comments': user_comments,
+        'comment_form': comment_form
+    })
 
 
 @login_required
 def cancel_booking(request, booking_id):
     booking = Booking.objects.get(id=booking_id, user=request.user)
     if booking.can_cancel():
-        booking.delete()
-        # Inform user of successful cancellation
+        booking.cancel()
+        messages.success(request, "Booking cancelled successfully.")
     else:
-        # Inform user that cancellation is not possible
-        return redirect('view_bookings')
+        messages.error(
+            request, "Cancellation is not allowed less than 24 hours in advance.")
+    
+    return redirect('view_bookings')
 
+
+@login_required
+def add_comment(request):
+    if request.method == 'POST':
+        comment_form = CommentForm(request.POST)
+        if comment_form.is_valid():
+            comment = comment_form.save(commit=False)
+            comment.name = request.user.username
+            comment.email = request.user.email
+            comment.save()
+            return redirect('view_bookings')
+    else:
+        return redirect('view_bookings')
