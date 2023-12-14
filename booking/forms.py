23742b810ccc82a86f6c5697bc29484daa3ea3a7
DietkeSt <>
2023-12-14 21:04:01 +0000

    Adjust CommentForm fields

--- booking/forms.py
+++ booking/forms.py
@@ -5,7 +5,7 @@ from django import forms
 class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
-        fields = ('body',)
+        fields = ('service', 'body',)
 
 
 class BookingForm(forms.ModelForm):
