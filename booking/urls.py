4e157388ef17e9a43c7b7b797b120bb032067859
DietkeSt <>
2023-12-14 21:01:55 +0000

    Adjust booking URLS path

--- booking/urls.py
+++ booking/urls.py
@@ -1,13 +1,15 @@
 from . import views
-from .views import ServiceList, ServiceDetail, book_service, view_bookings, cancel_booking
+from .views import ServiceList, ServiceDetail, book_service, view_bookings, cancel_booking, add_comment, BookingView
 from django.urls import path
 
 
 urlpatterns = [
     path('', ServiceList.as_view(), name='home'),
-    path('<slug:slug>/', ServiceDetail.as_view(), name='service_detail'),
     path('book/<int:service_id>/', book_service, name='book_service'),
     path('bookings/', view_bookings, name='view_bookings'),
-    path('cancel-booking/<int:booking_id>/', cancel_booking,
-         name='cancel_booking'),
+    path('cancel-booking/<int:booking_id>/',
+         cancel_booking, name='cancel_booking'),
+    path('book/<int:service_id>/', BookingView.as_view(), name='book_service'),
+    path('add-comment/', views.add_comment, name='add_comment'),
+    path('<slug:slug>/', ServiceDetail.as_view(), name='service_detail'),
 ]
