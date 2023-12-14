1cb9ae88b6ff8d7d91f45afba92ef4349f0f2673
DietkeSt <>
2023-12-14 22:42:32 +0000

    Add get-time-slots URL

--- booking/urls.py
+++ booking/urls.py
@@ -1,5 +1,5 @@
 from . import views
-from .views import ServiceList, ServiceDetail, book_service, view_bookings, cancel_booking, add_comment, BookingView
+from .views import ServiceList, ServiceDetail, book_service, view_bookings, cancel_booking, add_comment, get_time_slots, BookingView
 from django.urls import path
 
 
@@ -11,5 +11,6 @@ urlpatterns = [
          cancel_booking, name='cancel_booking'),
     path('book/<int:service_id>/', BookingView.as_view(), name='book_service'),
     path('add-comment/', views.add_comment, name='add_comment'),
+    path('get-time-slots/', views.get_time_slots, name='get_time_slots'),
     path('<slug:slug>/', ServiceDetail.as_view(), name='service_detail'),
 ]
