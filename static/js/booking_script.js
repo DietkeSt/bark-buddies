381e997210cafd07120fdcce5a04614639be6da6
DietkeSt <>
2023-12-17 17:26:52 +0000

    Remove non used function for timeslots

--- static/js/booking_script.js
+++ static/js/booking_script.js
@@ -1,33 +1,3 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const startDateField = document.getElementById('id_start_date');
-    const endDateField = document.getElementById('id_end_date');
-
-    function updateTimeSlots() {
-        const startDate = startDateField.value;
-        const endDate = endDateField.value;
-        if (startDate && endDate) {
-            fetch(`/get-time-slots/?start_date=${startDate}&end_date=${endDate}`)
-                .then(response => response.json())
-                .then(data => {
-                    const timeSlotField = document.getElementById('id_time_slot');
-                    timeSlotField.innerHTML = ''; // Clear existing options
-                    data.slots.forEach(slot => {
-                        const option = document.createElement('option');
-                        option.value = slot.id;
-                        option.textContent = slot.display;
-                        if (slot.full) {
-                            option.disabled = true; // Disable the option if the slot is full
-                        }
-                        timeSlotField.appendChild(option);
-                    });
-                });
-        }
-    }
-
-    startDateField.addEventListener('change', updateTimeSlots);
-    endDateField.addEventListener('change', updateTimeSlots);
-});
-
 $(window).on("load", function () {
     $(".slider").slick({
         slidesToShow: 3,
