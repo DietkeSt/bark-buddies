bd7de17bc276ed8f22688ce4d5de2dc6293a58c8
DietkeSt <>
2023-12-19 19:20:48 +0000

    Adjust URLs for class based functions

--- booking/urls.py
+++ booking/urls.py
@@ -1,14 +1,13 @@
 from . import views
-from .views import ServiceList, ServiceDetail, book_service, view_bookings, cancel_booking, add_comment
+from .views import ServiceList, ServiceDetail, BookServiceView, BookingsView, CancelBookingView, AddCommentView
 from django.urls import path
 
 
 urlpatterns = [
     path('', ServiceList.as_view(), name='home'),
-    path('book/<int:service_id>/', book_service, name='book_service'),
-    path('bookings/', view_bookings, name='view_bookings'),
-    path('cancel-booking/<int:booking_id>/',
-         cancel_booking, name='cancel_booking'),
-    path('add-comment/', views.add_comment, name='add_comment'),
+    path('book/<int:service_id>/', BookServiceView.as_view(), name='book_service'),
+    path('bookings/', BookingsView.as_view(), name='view_bookings'),
+    path('cancel-booking/<int:booking_id>/', CancelBookingView.as_view(), name='cancel_booking'),
+    path('add-comment/', AddCommentView.as_view(), name='add_comment'),
     path('<slug:slug>/', ServiceDetail.as_view(), name='service_detail'),
 ]
