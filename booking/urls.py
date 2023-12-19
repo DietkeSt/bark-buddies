abec4d739221369b4b748a43fb1949f0b311b915
DietkeSt <>
2023-12-19 20:35:36 +0000

    Add delete-bookings url

--- booking/urls.py
+++ booking/urls.py
@@ -1,5 +1,5 @@
 from . import views
-from .views import ServiceList, ServiceDetail, BookServiceView, BookingsView, CancelBookingView, AddCommentView
+from .views import ServiceList, ServiceDetail, BookServiceView, BookingsView, CancelBookingView, DeleteBookingView, AddCommentView
 from django.urls import path
 
 
@@ -8,6 +8,7 @@ urlpatterns = [
     path('book/<int:service_id>/', BookServiceView.as_view(), name='book_service'),
     path('bookings/', BookingsView.as_view(), name='view_bookings'),
     path('cancel-booking/<int:booking_id>/', CancelBookingView.as_view(), name='cancel_booking'),
+    path('delete-booking/<int:booking_id>/', DeleteBookingView.as_view(), name='delete_booking'),
     path('add-comment/', AddCommentView.as_view(), name='add_comment'),
     path('<slug:slug>/', ServiceDetail.as_view(), name='service_detail'),
 ]
