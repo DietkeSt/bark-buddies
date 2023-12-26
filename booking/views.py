47fb2d4c1f1ba70fb6bc8daaf01332c1cf79a735
DietkeSt <>
2023-12-26 12:01:38 +0000

    Streamline views by using model functions

--- booking/views.py
+++ booking/views.py
@@ -17,31 +17,24 @@ from django.utils import timezone
 
 class ServiceList(generic.ListView):
     model = Service
-    queryset = Service.objects.filter(status=1).order_by('title')
+    queryset = Service.get_active_services()
     template_name = 'index.html'
 
 
 class ServiceDetail(View):
 
     def get(self, request, slug, *args, **kwargs):
-        queryset = Service.objects.filter(status=1)
-        service = get_object_or_404(queryset, slug=slug)
-        comments = service.comments.filter(
-            approved=True).order_by('created_on')
+        service, comments = Service.get_service_with_comments(slug)
+        if not service:
+            return redirect('error_404')
 
-        booking_form = BookingForm()
-
-        return render(
-            request,
-            "service_detail.html",
-            {
-                "service": service,
-                "comments": comments,
-                "commented": False,
-                "comment_form": CommentForm(),
-                "booking_form": booking_form,
-            },
-        )
+        return render(request, "service_detail.html", {
+            "service": service,
+            "comments": comments,
+            "commented": False,
+            "comment_form": CommentForm(),
+            "booking_form": BookingForm(),
+        })
 
     def post(self, request, slug, *args, **kwargs):
         queryset = Service.objects.filter(status=1)
@@ -73,16 +66,6 @@ class ServiceDetail(View):
 
 
 class BookServiceView(LoginRequiredMixin, View):
-    @staticmethod
-    def has_overlapping_bookings(start_date, end_date, time):
-        overlapping_bookings = Booking.objects.filter(
-            start_date=start_date,
-            end_date=end_date,
-            time=time,
-            is_cancelled=False
-        )
-        return overlapping_bookings.exists()
-
     def post(self, request, service_id):
         service = get_object_or_404(Service, id=service_id)
         form = BookingForm(request.POST)
@@ -91,28 +74,12 @@ class BookServiceView(LoginRequiredMixin, View):
             booking_start = booking.start_date
             booking_end = booking.end_date
 
-            # Check for unavailable periods
-            unavailable_periods = Availability.objects.filter(
-                unavailable_from__lt=booking_end,
-                unavailable_to__gt=booking_start
-            )
-            if unavailable_periods.exists():
-                unavailable_str = ", ".join([
-                    f"{period.unavailable_from.strftime('%d/%m/%Y')} to {period.unavailable_to.strftime('%d/%m/%Y')}"
-                    for period in unavailable_periods
-                ])
-                messages.error(
-                    request,
-                    f'Dates {unavailable_str} are unavailable for booking.'
-                )
+            if not Booking.is_period_available(booking_start, booking_end):
+                messages.error(request, 'Selected dates are unavailable.')
                 return HttpResponseRedirect(reverse('service_detail', args=[service.slug]))
 
-            # Check for overlapping bookings
-            if BookServiceView.has_overlapping_bookings(booking.start_date, booking.end_date, booking.time):
-                messages.error(
-                    request,
-                    'The selected time is already booked for those dates.'
-                )
+            if Booking.has_overlapping_bookings(booking_start, booking_end, booking.time):
+                messages.error(request, 'Selected time is already booked.')
                 return HttpResponseRedirect(reverse('service_detail', args=[service.slug]))
 
             booking.user = request.user
@@ -126,18 +93,12 @@ class BookServiceView(LoginRequiredMixin, View):
 
 class BookingsView(LoginRequiredMixin, View):
     def get(self, request):
-        # Get current date
-        current_date = timezone.now().date()
-
-        # Booking filter to only show future bookings
-        bookings = Booking.objects.filter(
-            user=request.user, 
-            start_date__gte=current_date, 
-        ).order_by('start_date')
-
-        comments = Comment.objects.filter(name=request.user.username, approved=True)
+        bookings = Booking.get_future_bookings_for_user(request.user)
+        comments = Comment.objects.filter(
+            name=request.user.username, approved=True)
         comment_form = CommentForm()
-        commented = Comment.objects.filter(name=request.user.username, approved=False).exists()
+        commented = Comment.objects.filter(
+            name=request.user.username, approved=False).exists()
 
         return render(request, 'view_bookings.html', {
             'bookings': bookings,
@@ -149,39 +110,29 @@ class BookingsView(LoginRequiredMixin, View):
     def post(self, request):
         comment_form = CommentForm(request.POST)
         if comment_form.is_valid():
-            comment = comment_form.save(commit=False)
-            comment.name = request.user.username
-            comment.save()
+            Comment.create_comment(request.user, comment_form.cleaned_data)
             messages.success(request, 'Review added successfully.')
             return redirect('view_bookings')
         else:
-            # Handle not valid form
-            bookings = Booking.objects.filter(user=request.user).order_by('start_date')
-            comments = Comment.objects.filter(name=request.user.username, approved=True)
-            commented = Comment.objects.filter(name=request.user.username, approved=False).exists()
-            return render(request, 'view_bookings.html', {
-                'bookings': bookings,
-                'comments': comments,
-                'comment_form': comment_form,
-                'commented': commented
-            })
+            return self.get(request)
 
 
 
 class CancelBookingView(LoginRequiredMixin, View):
     def post(self, request, booking_id):
         booking = get_object_or_404(Booking, id=booking_id, user=request.user)
-        if booking.can_cancel():
-            booking.cancel()
+        if booking.cancel_booking():
             messages.success(request, "Booking cancelled successfully.")
         else:
-            messages.error(request, "Cancellation not allowed less than 24 hours in advance.")
+            messages.error(
+                request, "Cancellation not allowed less than 24 hours in advance.")
         return redirect('view_bookings')
 
 
 class DeleteBookingView(LoginRequiredMixin, View):
     def post(self, request, booking_id):
-        booking = get_object_or_404(Booking, id=booking_id, user=request.user, is_cancelled=True)
+        booking = get_object_or_404(
+            Booking, id=booking_id, user=request.user, is_cancelled=True)
         booking.delete()
         messages.success(request, "Booking deleted successfully.")
         return redirect('view_bookings')
