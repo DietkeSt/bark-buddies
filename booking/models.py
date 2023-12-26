4d786025add2f893605f85fd847dc1120f7bdffa
DietkeSt <>
2023-12-26 12:00:43 +0000

    Change meta options, alter body and content field

--- booking/models.py
+++ booking/models.py
@@ -16,14 +16,24 @@ STATUS = (
 class Service(models.Model):
     title = models.CharField(max_length=200, unique=True)
     slug = models.SlugField(max_length=200, unique=True)
-    content = models.TextField()
+    content = models.TextField(max_length=400)
     price = models.DecimalField(max_digits=10, decimal_places=2, default=0)
     duration = models.DurationField(default=datetime.timedelta(hours=1))
     featured_image = CloudinaryField('image', default='placeholder')
     status = models.IntegerField(choices=STATUS, default=0)
 
-    class Meta:
-        ordering = ['title']
+    @classmethod
+    def get_active_services(cls):
+        return cls.objects.filter(status=1).order_by('title')
+
+    @classmethod
+    def get_service_with_comments(cls, slug):
+        service = cls.objects.filter(slug=slug, status=1).first()
+        if service:
+            comments = service.comments.filter(
+                approved=True).order_by('created_on')
+            return service, comments
+        return None, None
 
     def save(self, *args, **kwargs):
         if not self.slug:
@@ -47,7 +57,7 @@ class Comment(models.Model):
         Service, on_delete=models.CASCADE, related_name='comments')
     name = models.CharField(max_length=80)
     email = models.EmailField()
-    body = models.TextField()
+    body = models.TextField(max_length=400)
     created_on = models.DateTimeField(auto_now_add=True)
     approved = models.BooleanField(default=False)
 
@@ -56,6 +66,12 @@ class Comment(models.Model):
 
     def __str__(self):
         return f"Comment {self.body} by {self.name}"
+
+    @classmethod
+    def create_comment(cls, user, form_data):
+        comment = cls(name=user.username, email=user.email, **form_data)
+        comment.save()
+        return comment
    
 
 class Booking(models.Model):
@@ -70,10 +86,25 @@ class Booking(models.Model):
     def __str__(self):
         return f'{self.user} booked {self.service} from {self.start_date} to {self.end_date} at {self.time.strftime("%I:%M %p")}'
 
-    # Check if the booking can be cancelled
-    def can_cancel(self):
-        return timezone.now() <= timezone.make_aware(datetime.datetime.combine(self.start_date, datetime.time.min)) - datetime.timedelta(days=1)
+    @staticmethod
+    def has_overlapping_bookings(start_date, end_date, time):
+        return Booking.objects.filter(start_date=start_date, end_date=end_date, time=time, is_cancelled=False).exists()
 
-    def cancel(self):
-        self.is_cancelled = True
-        self.save()
+    @staticmethod
+    def is_period_available(start_date, end_date):
+        return not Availability.objects.filter(unavailable_from__lt=end_date, unavailable_to__gt=start_date).exists()
+
+    def can_cancel(self):
+        return timezone.now() < self.start_date - timedelta(hours=24)
+
+    def cancel_booking(self):
+        if self.can_cancel():
+            self.is_cancelled = True
+            self.save()
+            return True
+        return False
+
+    @classmethod
+    def get_future_bookings_for_user(cls, user):
+        current_date = timezone.now().date()
+        return cls.objects.filter(user=user, start_date__gte=current_date).order_by('start_date')
