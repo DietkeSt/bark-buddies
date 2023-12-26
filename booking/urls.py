13dbc0e4e3836bc836e22fc937801bc9e8dee5d6
DietkeSt <>
2023-12-26 19:56:07 +0000

    Add url path for unavailable times

--- booking/urls.py
+++ booking/urls.py
@@ -1,10 +1,12 @@
 from . import views
-from .views import ServiceList, ServiceDetail, BookServiceView, BookingsView, CancelBookingView, DeleteBookingView, AddCommentView
+from .views import ServiceList, ServiceDetail, BookServiceView, BookingsView, CancelBookingView, DeleteBookingView, AddCommentView, GetUnavailableTimes
 from django.urls import path
 
 
 urlpatterns = [
     path('', ServiceList.as_view(), name='home'),
+    path('get-unavailable-times/', GetUnavailableTimes.as_view(),
+         name='get_unavailable_times'),
     path('book/<int:service_id>/', BookServiceView.as_view(), name='book_service'),
     path('bookings/', BookingsView.as_view(), name='view_bookings'),
     path('cancel-booking/<int:booking_id>/', CancelBookingView.as_view(), name='cancel_booking'),
