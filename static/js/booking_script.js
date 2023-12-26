37c96d5532e77960f01924bbe8d93e67d7999c2d
DietkeSt <>
2023-12-26 19:54:48 +0000

    Add check for unavailable dates and update the options

--- static/js/booking_script.js
+++ static/js/booking_script.js
@@ -1,5 +1,4 @@
 $(window).on("load", function () {
-
     // Slick slider for service page
     $(".slider").slick({
         slidesToShow: 3,
@@ -29,6 +28,51 @@ $(window).on("load", function () {
             alert.close();
         }
     }, 2500);
+
+    // Check for unavailable times and update them
+    $('#id_start_date, #id_end_date').change(function () {
+        const startDate = document.getElementById('id_start_date').value;
+        const endDate = document.getElementById('id_end_date').value;
+
+        if (startDate && endDate) {
+            fetch(`/get-unavailable-times/?start_date=${startDate}&end_date=${endDate}`)
+                .then(response => response.json())
+                .then(data => {
+                    const unavailableTimes = data.unavailable_times;
+                    updateAvailableTimes(document.getElementById('id_time'), unavailableTimes);
+                });
+        }
+    });
+
+    function updateAvailableTimes(select, unavailableTimes) {
+        const options = select.options;
+        let currentValue = select.value;
+
+        // Convert the current value to 'HH:MM' format for comparison
+        if (currentValue.length === 8) {
+            currentValue = currentValue.substr(0, 5);
+        }
+
+        // Flag to check if the current value is available
+        let isCurrentValueAvailable = true;
+
+        for (let i = 0; i < options.length; i++) {
+            // Strip off seconds from the option value (HH:MM:SS to HH:MM)
+            const optionValue = options[i].value.substr(0, 5);
+            const isUnavailable = unavailableTimes.includes(optionValue);
+            options[i].disabled = isUnavailable;
+
+            // Check if the current value is among the unavailable times
+            if (optionValue === currentValue && isUnavailable) {
+                isCurrentValueAvailable = false;
+            }
+        }
+
+        // If the current value is unavailable, reset the select
+        if (!isCurrentValueAvailable) {
+            select.value = ''; // Reset to a neutral or NULL value
+        }
+    }
 
     // Initialize the checkbox handler when the modal is shown
     $('#bookingModal').on('shown.bs.modal', function () {
