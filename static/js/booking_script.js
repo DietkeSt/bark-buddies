e7401933d105286d05b770996b97371e42ceab4b
DietkeSt <>
2023-12-26 16:18:15 +0000

    Correct OnDay checkbox function to make sure enddate is set and not disabled

--- static/js/booking_script.js
+++ static/js/booking_script.js
@@ -30,30 +30,33 @@ $(window).on("load", function () {
         }
     }, 2500);
 
-    // Function to handle the checkbox change
-    function handleOneDayCheckbox(checkbox) {
-        var endDateInput = document.getElementById("id_end_date");
-        if (checkbox.checked) {
-            endDateInput.disabled = true;
-        } else {
-            endDateInput.disabled = false;
-        }
-    }
-
     // Initialize the checkbox handler when the modal is shown
     $('#bookingModal').on('shown.bs.modal', function () {
         initOneDayCheckboxHandler();
     });
 
-    // Setup the checkbox handler
     function initOneDayCheckboxHandler() {
         var checkbox = document.getElementById("id_just_one_day");
-        if (checkbox) {
-            checkbox.onchange = function () {
-                handleOneDayCheckbox(this);
-            };
-            // Run once to set initial state
-            handleOneDayCheckbox(checkbox);
+        var startDateInput = document.getElementById("id_start_date");
+        var endDateInput = document.getElementById("id_end_date");
+
+        // Function to update the end date based on checkbox and start date
+        function updateEndDate() {
+            if (checkbox.checked && startDateInput.value) {
+                endDateInput.value = startDateInput.value;
+                endDateInput.readOnly = true;
+            } else {
+                endDateInput.readOnly = false;
+            }
+        }
+
+        if (checkbox && startDateInput && endDateInput) {
+            // Attach change event to checkbox and start date input
+            checkbox.onchange = updateEndDate;
+            startDateInput.onchange = updateEndDate;
+
+            // Initialize with current state
+            updateEndDate();
         }
     }
 });
