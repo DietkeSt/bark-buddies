216224aece1f1a1e3e55a16588d591fb97af174c
DietkeSt <>
2023-12-26 12:51:09 +0000

    Add one-day option to BookingForm

--- booking/forms.py
+++ booking/forms.py
@@ -9,6 +9,8 @@ class CommentForm(forms.ModelForm):
 
 
 class BookingForm(forms.ModelForm):
+    just_one_day = forms.BooleanField(required=False, label='Just one day')
+
     class Meta:
         model = Booking
         fields = ['start_date', 'end_date', 'time', 'comments']
@@ -17,3 +19,13 @@ class BookingForm(forms.ModelForm):
             'end_date': forms.DateInput(attrs={'type': 'date'}),
             'time': forms.TimeInput(attrs={'type': 'time'}),
         }
+
+    def clean(self):
+        cleaned_data = super().clean()
+        just_one_day = cleaned_data.get('just_one_day')
+        start_date = cleaned_data.get('start_date')
+
+        if just_one_day and start_date:
+            cleaned_data['end_date'] = start_date
+
+        return cleaned_data
