1a794de101f4c5333960364bd89258ae222410ad
DietkeSt <>
2023-12-27 22:17:02 +0000

    Add CommentAdmin so admin can control comments

--- reviews/admin.py
+++ reviews/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from .models import Comment
 
-# Register your models here.
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+
+    list_display = ('name', 'body', 'service', 'created_on', 'approved')
+    list_filter = ('approved', 'created_on')
+    search_fields = ('name', 'email', 'body')
+    actions = ['approve_comments']
+
+    def get_form(self, request, obj=None, **kwargs):
+        form = super().get_form(request, obj, **kwargs)
+        form.base_fields['email'].required = False
+        return form
+
+    def approve_comments(self, request, queryset):
+        for obj in queryset:
+            obj.approve()
+
+    approve_comments.short_description = "Approve selected comments"
