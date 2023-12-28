05ab3de0463e7ec310af27ba2894763ea9fff43a
DietkeSt <>
2023-12-28 23:29:34 +0000

    Adjust the home view to display comments and comment form

--- home/views.py
+++ home/views.py
@@ -1,10 +1,21 @@
 # home/views.py
 from django.shortcuts import render
 from django.views import generic, View
+from django.views.generic import ListView
 from booking.models import Service
+from reviews.models import Comment
+from reviews.forms import CommentForm
 
 
-class HomeView(generic.ListView):
+class HomeView(ListView):
     model = Service
-    queryset = Service.active.active_services()
     template_name = 'home.html'
+    context_object_name = 'service_list'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['comments'] = Comment.objects.filter(
+            approved=True)[:10]
+        context['comment_form'] = CommentForm(
+        ) if self.request.user.is_authenticated else None
+        return context
