90bb4aa79efbfb29c36b754b6b65edc7e049b625
DietkeSt <>
2023-12-29 00:22:30 +0000

    Add SubmitHomeReview into URLs

--- home/urls.py
+++ home/urls.py
@@ -1,9 +1,12 @@
 # home/urls.py
 from django.urls import path
-from .views import HomeView
+from .views import HomeView, SubmitHomeReview
 
 urlpatterns = [
     path(
         '', HomeView.as_view(), name='home'
     ),
+    path(
+        'submit-review/', SubmitHomeReview.as_view(), name='submit_home_review'
+        ),
 ]
