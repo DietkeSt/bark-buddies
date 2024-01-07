4d8636e1829cd8f01981167a87f33e6aa69639d5
DietkeSt <>
2024-01-07 14:31:45 +0000

    Add function for published services only

--- context_processors.py
+++ context_processors.py
@@ -4,3 +4,8 @@ from booking.models import Service
 def services_processor(request):
     services = Service.objects.all()
     return {'services': services}
+
+
+def add_published_services_to_context(request):
+    published_services = Service.objects.filter(status=1)
+    return {'published_services': published_services}
