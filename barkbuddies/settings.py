e8bd1fc75ca682062a3871809247186bd8b15870
DietkeSt <>
2024-01-07 14:39:28 +0000

    Add context processor for published services to Templates

--- barkbuddies/settings.py
+++ barkbuddies/settings.py
@@ -102,6 +102,7 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
                 'context_processors.services_processor',
+                'context_processors.add_published_services_to_context',
             ],
         },
     },
