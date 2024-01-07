07ca6de7c4c8ba6320d443b905fadbf5ffd5db07
DietkeSt <>
2024-01-07 14:35:41 +0000

    Add filter to only show published comments in comment form

--- reviews/forms.py
+++ reviews/forms.py
@@ -1,4 +1,5 @@
 from .models import Comment
+from booking.models import Service
 from django import forms
 
 
@@ -6,3 +7,8 @@ class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
         fields = ('service', 'body',)
+
+    def __init__(self, *args, **kwargs):
+        super(CommentForm, self).__init__(*args, **kwargs)
+        # Set the queryset for the 'service' field to only include published services
+        self.fields['service'].queryset = Service.objects.filter(status=1)
