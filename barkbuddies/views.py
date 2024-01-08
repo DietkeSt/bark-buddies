601ab2aa0621d3aae2794ccb5de541f0ab8c9939
DietkeSt <>
2024-01-08 22:28:44 +0000

    Simplify views

--- barkbuddies/views.py
+++ barkbuddies/views.py
@@ -1,9 +1,7 @@
 from django.shortcuts import render
 
 def custom_404(request, exception):
-    context = {'user': request.user} if hasattr(request, 'user') else {}
-    return render(request, 'errors/404.html', context, status=404)
+    return render(request, 'errors/404.html', status=404)
 
 def custom_500(request):
-    context = {'user': request.user} if hasattr(request, 'user') else {}
-    return render(request, 'errors/500.html', context, status=500)
+    return render(request, 'errors/500.html', status=500)
