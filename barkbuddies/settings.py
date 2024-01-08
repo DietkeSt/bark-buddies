7928e34cd5c816212d42c9afcf47385cf55bcd47
DietkeSt <>
2024-01-08 21:59:34 +0000

    Set debug to false

--- barkbuddies/settings.py
+++ barkbuddies/settings.py
@@ -27,14 +27,11 @@ development = os.environ.get('DEVELOPMENT', False)
 BASE_DIR = Path(__file__).resolve().parent.parent
 TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
+# Secret Key
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get("DEBUG") == "True"
+# Debug
+DEBUG = os.environ.get("DEBUG") == "False"
 
 X_FRAME_OPTIONS = 'SAMEORIGIN'
 
