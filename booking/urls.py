a0aed906a152238f2ebe4eed05cc9e6e5e3b118f
DietkeSt <>
2024-01-10 17:29:46 +0000

    Add edit booking url

--- booking/urls.py
+++ booking/urls.py
@@ -1,7 +1,8 @@
 # booking/urls.py
 from .views import (
     ServiceDetail, BookServiceView, BookingsView,
-    CancelBookingView, DeleteBookingView, AddCommentView, GetUnavailableTimes
+    CancelBookingView, DeleteBookingView, AddCommentView, GetUnavailableTimes,
+    EditBookingView
 )
 from django.urls import path
 
@@ -19,6 +20,7 @@ urlpatterns = [
          CancelBookingView.as_view(), name='cancel_booking'),
     path('delete-booking/<int:booking_id>/',
          DeleteBookingView.as_view(), name='delete_booking'),
+    path('edit-booking/<int:booking_id>/', EditBookingView.as_view(), name='edit_booking'),
     path('add-comment/', AddCommentView.as_view(), name='add_comment'),
     path('<slug:slug>/', ServiceDetail.as_view(), name='service_detail'),
 ]
